bf0b2c7474d87a27d703db906b562e7242d559ff
GV <patiencebro@gmail.com>
2020-02-08 18:02:51 -0500

    l

--- Synth/JuceLibraryCode/JuceHeader.h
+++ Synth/JuceLibraryCode/JuceHeader.h
@@ -17,7 +17,6 @@
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_formats/juce_audio_formats.h>
-#include <juce_audio_plugin_client/juce_audio_plugin_client.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_core/juce_core.h>
@@ -48,7 +47,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "Synth";
+    const char* const  projectName    = "NewProject";
     const char* const  companyName    = "";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
