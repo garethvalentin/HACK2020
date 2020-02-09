c354ec1d57474fb280703d18b67188793b4d0c78
GV <patiencebro@gmail.com>
2020-02-09 05:40:59 -0500

    q

---
+++ newnew/newnew.ino
@@ -0,0 +1,122 @@
+
+int D2 = 2;
+int D3 = 3;
+int D4 = 4;
+
+int D5 = 5;
+int D6 = 6;
+int D7 = 7;
+int D8 = 8;
+
+int AN0 = 0;
+int AN1 = 1;
+int x_value = 0;
+int y_value = 0;
+int x_init = 0;                 
+int y_init = 0;                
+
+int length = 15;         /* the number of notes */
+char notes[] = "ccggaagffeeddc ";
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+int tempo = 300;
+
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  pinMode(D2, INPUT);
+  pinMode(D3, INPUT);
+  pinMode(D4, INPUT);
+
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT);
+  
+  x_init = analogRead(AN0);  
+  delay(50);            
+  y_init = analogRead(AN1);  
+}
+
+int treatValue(int data) {
+  return (data * 9 / 1024) + 48;
+}
+
+
+void playTone(int tone, int duration) {
+    for (long i = 0; i < duration * 1000L; i += tone * 2) {
+        digitalWrite(D8, HIGH);
+        delayMicroseconds(tone);
+        digitalWrite(D8, LOW);
+        delayMicroseconds(tone);
+    }
+}
+
+void playNote(char note, int duration) {
+    char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+    int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+
+    // play the tone corresponding to the note name
+    for (int i = 0; i < 8; i++) {
+        if (names[i] == note) {
+            playTone(tones[i], duration);
+        }
+    }
+}
+
+
+void loop() {
+
+    if(Serial.available()){
+        Serial.print("WrrrrrrrrWWWWW  ");
+      int data = Serial.read();
+      if(data >= 0.75) {
+        Serial.print("WWWWWWWWWWWWWWW  ");
+        Serial.println(data);
+        digitalWrite(D7, HIGH); 
+      }
+      else{
+        Serial.print("WWWWqqqqqqqqqWWWWW  ");
+        Serial.println(data);
+        digitalWrite(D7, LOW);
+      }
+    }
+
+/*
+    for(int i = 0; i < length; i++) {
+        if(notes[i] == ' ') {
+            delay(beats[i] * tempo);
+        } else {
+            playNote(notes[i], beats[i] * tempo);
+        }
+        delay(tempo / 2);    
+    }
+*/
+
+    
+  int in_D2 = digitalRead(D2);
+  int in_D3 = digitalRead(D3);
+  int in_D4 = digitalRead(D4);
+
+  /*
+  Serial.print("ddddd  ");
+  Serial.println(in_D2);
+  Serial.println(in_D3);
+  Serial.println(in_D4);
+*/
+
+  //digitalWrite(8, HIGH);
+  //delay(1000);
+  //digitalWrite(8, LOW);
+  //delay(1000);
+
+
+  x_value = analogRead(AN0);  
+  delay(5);            
+  y_value = analogRead(AN1);  
+
+  //Serial.print(x_value - x_init);
+  //Serial.print("  mmm  ");
+  //Serial.println(y_value - y_init);
+ 
+}
