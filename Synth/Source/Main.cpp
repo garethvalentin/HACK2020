db29094f9c47fe166ece0286be3725a81f4b8832
wnam98 <49884140+wnam98@users.noreply.github.com>
2020-02-09 14:50:51 -0500

    New mods

--- Synth/Source/Main.cpp
+++ Synth/Source/Main.cpp
@@ -9,11 +9,11 @@
 #include <JuceHeader.h>
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//#include "SynthUsingMidiInputTutorial_01.h"
+#include "SynthUsingMidiInputTutorial_01.h"
 //#include "WavetableSynthTutorial_01.h"
 //#include "WavetableSynthTutorial_02.h"
 //#include "WavetableSynthTutorial_03.h"
-#include "WavetableSynthTutorial_04.h"
+//#include "WavetableSynthTutorial_04.h"
 
 class Application    : public JUCEApplication
 {
@@ -25,10 +25,10 @@ public:
         
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!
         
-        //return "SynthUsingMidiInputTutorial"; 
+        return "SynthUsingMidiInputTutorial"; 
         
         // uncomment if trying using wavetable synth
-        return "WavetableSynthTutorial"; 
+        //return "WavetableSynthTutorial"; 
     }
     const String getApplicationVersion() override    {
         
@@ -36,10 +36,10 @@ public:
         
         // uncomment if trying to use different wavetbale tutorial file
          
-        //return "1.0.0";
+        return "1.0.0";
         //return "2.0.0"; 
         //return "3.0.0"; 
-        return "4.0.0"; 
+        //return "4.0.0"; 
     }
 
     void initialise (const String&) override         { mainWindow.reset (new MainWindow ("SynthUsingMidiInputTutorial", new MainContentComponent(), *this)); }
